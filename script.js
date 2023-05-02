95ab3a09b3a335bc26d27ba689cbe60735e11b15
megamasterguido <guidozamussi@hotmail.com>
2023-05-02 17:45:19 -0300

    sprint-2

--- script.js
+++ script.js
@@ -1,80 +1,152 @@
 const fs = require("fs")
+
 class ProductManager{
     constructor(path){
         this.products = []
         this.path = path
+        this.lastId = 0
+        this.ks = this.init(this.path)
     }
 
-    async getProducts(){
+    init(path){
+        let file = fs.existsSync(path)
         let resp
-        await fs.promises.readFile(this.path, "utf-8")
-            .then(res => res == "" || res == "[]"? resp = "Not found" : resp = JSON.parse(res) )
-            .catch(() => resp = "getProducts: error")
-        return resp
-    }
-
-    async getProductById(id){
-        let resp = "getProductById: error"
-        let prods
-        await fs.promises.readFile(this.path, "utf-8")
-            .then(res => prods = JSON.parse(res))
-            .catch(() => prods = "Error")
-        
-        console.log(prods)
-
-        if(prods == "" || prods == "[]"){
-            resp = "Not found"
-        }
-        else if(prods == "Error"){
-            resp = "getProductById: error"
-        }
-        else{
-            resp = JSON.parse(prods)
-        }
-        
-        return resp
-    }
-
-    async addProduct(title, description, price, thumbnail, stock = 0){
-        let resp = "addProduct: error"
-        let vacio = await this.getProducts()
-
-        if(typeof(vacio) != "string"){
-            this.products = vacio
-
-            this.products.forEach(prod => {
-                if(prod.title == title){
-                    resp = "Ya existe un producto con esas propiedades en la lista"
+        if(file){
+            let archivo = fs.readFileSync(path, "UTF-8")
+            try{
+                this.products = JSON.parse(archivo)
+                resp = "Datos recuperados"
+                for(const prod in this.products){
+                    if(prod.id >= this.lastId){
+                        this.lastId = prod.id+1
+                    }
                 }
-            })
-            if(resp != "Ya existe un producto con esas propiedades en la lista"){
-                this.products.push({id: this.products.length+1, title: title, description: description, price: price, thumbnail: thumbnail, stock: stock})
-                resp = this.products.length
-                fs.promises.writeFile(this.path, JSON.stringify(this.products,null,2))
+            }
+            catch{
+                resp = "Datos irrecuperables"
             }
         }
-        else if(vacio == "[]" || vacio == "getProducts: error" || vacio == "Not found" ){
-            this.products.push({id: this.products.length+1, title: title, description: description, price: price, thumbnail: thumbnail, stock: stock})
-            resp = this.products.length
-            fs.promises.writeFile(this.path, JSON.stringify(this.products,null,2))
+        else{
+            fs.writeFileSync(path, "[]")
+            resp = "Archivo creado"
+        }
+        return resp
+    }
+
+    getProducts(){
+        let resp = "getProducts: error"
+        if(this.ks != "Datos irrecuperables"){
+            this.products.length == 0? resp = "Not found" : resp = this.products
+        }
+        return resp
+    }
+
+    getProductById(id){
+        let resp = "getProductById: error"
+        let find = this.products.find(prod => prod.id == id)
+        if(this.ks != "Datos irrecuperables"){
+            find == undefined? resp = "Not found" : resp = find
+        }
+        
+        return resp
+    }
+
+    alreadyIn(title){
+        let resp = false
+            this.products.forEach(prod => {
+                if(prod.title == title){
+                    resp = true
+                }
+            })
+        return resp
+    }
+
+    addProduct(title, description, price, thumbnail, stock = 0){
+        let resp = "addProduct: error"
+
+        if(this.ks != "Datos irrecuperables"){
+
+            if(title != undefined && description != undefined && price != undefined && thumbnail != undefined){
+                if(this.alreadyIn(title)){
+                    resp = "Ya existe este producto en la lista"
+                }
+                if(resp != "Ya existe este producto en la lista"){
+                    this.lastId++
+                    this.products.push({id: this.lastId, title: title, description: description, price: price, thumbnail: thumbnail, stock: stock})
+                    resp = this.lastId
+                    fs.writeFileSync(this.path, JSON.stringify(this.products,null,2))
+                }
+
+            }
         }
 
         return resp
     }
 
-    async updateProduct(id, data){
+    updateProduct(id, data){
+        let resp = "updateProduct: error"
+        if(this.ks != "Datos irrecuperables" && data.title != undefined && data.description != undefined && data.price != undefined && data.thumbnail != undefined){
+            let prod = this.products.find(producto => producto.id == id)
+            let repetido = this.products.find(producto => producto.title == data.title)
+            if(repetido != undefined && repetido != prod && prod != undefined){
+                resp = "Ya existe este producto en la lista"
+            }
+            else if(prod == undefined){
+                resp = "Not found"
 
+            }
+            else{
+                prod.title = data.title
+                prod.description = data.description
+                prod.price = data.price
+                prod.thumbnail = data.thumbnail
+
+                data.stock == undefined? prod.stock = prod.stock: prod.stock = data.stock
+
+                resp = "updateProduct: done"
+                fs.writeFileSync(this.path, JSON.stringify(this.products,null,2))
+            }
+
+        }
+        return resp
+    }
+
+    deleteProduct(id){
+        let resp = "deleteProduct: error"
+        if(this.ks != "Datos irrecuperables"){
+            let prod = this.products.find(producto => producto.id == id)
+            if(prod){
+                this.products.splice(this.products.indexOf(prod), 1)
+                resp = "deleteProduct: done"
+                fs.writeFileSync(this.path, JSON.stringify(this.products,null,2))
+            }
+            else{
+                resp = "Not found"
+            }
+        }
+        return resp
     }
 }
 
 async function pruebas(){
-    let prueba = new ProductManager("./Productos")
-    console.log("1", await prueba.getProducts())
-    console.log("2", await prueba.getProductById(1))
-    console.log("3", await prueba.addProduct("Eje Drean Original", "Eje Drean Excellent 166 original", 12000, "https://www.serviceitalia.com.ar/images/uploads/ecommerce/1718.jpg"))
-    console.log("4", await prueba.addProduct("Eje Drean Copia", "Eje Drean Excellent 166 original", 12000, "https://www.serviceitalia.com.ar/images/uploads/ecommerce/1718.jpg"))
-    console.log("5", await prueba.getProducts())
-    console.log("6", await prueba.getProductById(1))
+    let prueba = new ProductManager("./Productos.json")
+    console.log("1", prueba.init("./Productos.json"))
+    console.log("2", prueba.getProducts())
+    console.log("3", prueba.getProductById(4))
+    console.log("4", prueba.addProduct("Eje Drean Original", "Eje Drean Excellent 166 original", 12000, "https://www.serviceitalia.com.ar/images/uploads/ecommerce/1718.jpg"))
+    console.log("5", prueba.getProductById(1))
+    console.log("6", prueba.addProduct("Eje Drean Copia", "Eje Drean Excellent 166 copia", "https://www.serviceitalia.com.ar/images/uploads/ecommerce/1718.jpg"))
+    console.log("7", prueba.addProduct("Eje Drean Copia", "Eje Drean Excellent 166 original", 4000, "https://www.serviceitalia.com.ar/images/uploads/ecommerce/1718.jpg"))
+    console.log("8", prueba.getProducts())
+    console.log("9", prueba.updateProduct(2, {title: "Eje Drean Copia"}))
+    console.log("10", prueba.updateProduct(3, {title: "Eje Drean Copia", description:"Eje Drean Excellent 166 copia", price: 4000, thumbnail:"https://www.serviceitalia.com.ar/images/uploads/ecommerce/1718.jpg"}))
+    console.log("11", prueba.updateProduct(2, {title: "Eje Drean Copia", description:"Eje Drean Excellent 166 copia", price: 4000, thumbnail:"https://www.serviceitalia.com.ar/images/uploads/ecommerce/1718.jpg"}))
+    console.log("12", prueba.getProducts())
+    console.log("13", prueba.deleteProduct(1))
+    console.log("14", prueba.deleteProduct(3))
+    console.log("15", prueba.getProducts())
+    console.log("16", prueba.addProduct("Eje Drean Original", "Eje Drean Excellent 166 original", 12000, "https://www.serviceitalia.com.ar/images/uploads/ecommerce/1718.jpg"))
+    console.log("17", prueba.getProducts())
 }
 
 pruebas()
