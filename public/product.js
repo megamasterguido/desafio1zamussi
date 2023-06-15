e4015cf6039338a3530ca86f37d04aeabe62211c
megamasterguido <guidozamussi@hotmail.com>
2023-06-15 08:07:23 -0300

    sprint 5

---
+++ public/product.js
@@ -0,0 +1,51 @@
+let id = window.location.href.substring(31)
+
+let prod
+
+fetch("http://localhost:8080/api/products/"+id)
+    .then(resp => resp.json())
+    .then(resp => prod = resp.response)
+    .then(() => landing(prod))
+    .then(() => {
+        let button
+        let input
+
+        button = document.getElementById("add_cart__button")
+            button.addEventListener("click", () => {
+                input = document.getElementById("add_cart__input")
+                fetch("http://localhost:8080/api/carts/1/products/"+ id +"/"+input.value, {method: "PUT"})
+                .then(resp => resp.json())
+                .then(resp => {
+                    if(typeof(resp.response) == "string"){
+                        alert(resp.response)
+                    }
+                    else{
+                        socket.emit("cart_update")
+                    }
+                })
+                .catch(err => console.error(err))
+            })
+        }
+    )
+    .catch(err => console.error(err))
+
+function landing(prod){
+    let resp = `<div class="product">
+    <div class="product_card__id">ID: ${prod.id}</div>
+    <div class="product_card__title">${prod.title}</div>
+    <div class="lateral">
+        <img src="${prod.thumbnail}" alt="Miniatura del producto" class="product_card__thumbnail">
+        <p class="product_card__description">${prod.description}</p>
+    </div>
+    <div class="abajo">
+        <div class="product_card__price">$${prod.price}</div>
+        <div class="add_cart">
+            <input min="0" type="number" id="add_cart__input">
+            <button id="add_cart__button" for="add_cart__input">AGREGAR AL CARRITO</button>
+        </div>
+    </div>
+</div>`
+
+    let main = document.getElementsByTagName("main")[0]
+    main.innerHTML += resp
+}
--- public/products.js
+++ public/products.js
@@ -30,11 +30,13 @@ fetch("http://localhost:8080/api/products")
 function card (prod){
     let resp = `
                 <div class="product_card">
+                    <a href="/products/${prod.id}">
                     <div class="product_card__id">ID: ${prod.id}</div>
                     <div class="product_card__title">${prod.title}</div>
                     <div class="product_card__price">$${prod.price}</div>
                     <img src="${prod.thumbnail}" alt="Miniatura del producto" class="product_card__thumbnail">
                     <p class="product_card__description">${prod.description}</p>
+                    </a>
                     <div class="add_cart">
                         <input min="0" type="number" id="add_cart__input${prod.id}">
                         <button id="add_cart__button${prod.id}" for="add_cart__input${prod.id}">AGREGAR AL CARRITO</button>
